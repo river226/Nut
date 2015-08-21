3e72015c39fa054e701b7978d5574c402f70e1c9
river <river226@gmail.com>
2015-08-20 23:02:36 -0500

    Read in XML

--- ain/tolva/nut/backend/AddPlugin.java
+++ ain/tolva/nut/backend/AddPlugin.java
@@ -1,6 +1,9 @@
 package ain.tolva.nut.backend;
 
 import java.awt.MenuItem;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Stack;
 import javax.xml.parsers.*;
@@ -42,34 +45,94 @@ public class AddPlugin{
 	}
 
 	public boolean empty() {
+		if(!hasRun) readInPlugins();
 		return tooAddPlugins.empty();
 	}
 
 	// TODO implement reading and writing plugin data
-
-	public boolean readInPlugins() {
+	// http://stackoverflow.com/questions/7373567/java-how-to-read-and-write-xml-files/7373596#7373596
+	public void readInPlugins() {
 		// Throws added plugins on the TooAddPlugins Stack
 		if(!hasRun) {// if this has run once there is nothing to do
-			Document dom;
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
 			try {
+				Document dom;
+				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 				DocumentBuilder db = dbf.newDocumentBuilder();
-				dom = db.parse("TODO");
+				dom = db.parse(new File("plist.xml"));
 
-				NodeList doc = dom.getElementsByTagName("plugin");
-
-				for(int i = 0; i < doc.getLength(); i++) {
-					// TODO: process plugin
+				if (dom != null) {
+					Element ele = dom.getDocumentElement();
+					if (ele != null) {
+						NodeList nl = ele.getElementsByTagName("location");
+						
+						for(int i = 0; i < nl.getLength(); i++) {
+							String loc = nl.item(i).getFirstChild().getNodeValue();
+							tooAddPlugins.push(addJar(loc));
+						}
+					}
 				}
-			} catch (Exception e) {
+
+			} catch (SAXException 
+					| IOException 
+					| ParserConfigurationException e) {
 				// Log
+				System.out.print(e.getMessage());
+			} finally {
+				hasRun = true;	
 			}
-				
-			if(!this.empty()) return true;
 		}
-		return false;
 	}
 	
+	private NutPlugin addJar(String loc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void saveToXML(String xml) {
+	    Document dom;
+	    Element e = null;
+
+	    // instance of a DocumentBuilderFactory
+	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	    try {
+	        // use factory to get an instance of document builder
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+	        // create instance of DOM
+	        dom = db.newDocument();
+
+	        // create the root element
+	        Element rootEle = dom.createElement("roles");
+
+	        // create data elements and place them under root
+	        //e = dom.createElement("role1");
+	        //e.appendChild(dom.createTextNode(role1));
+	        //rootEle.appendChild(e);
+
+	        dom.appendChild(rootEle);
+
+	        try {
+	            Transformer tr = TransformerFactory.newInstance().newTransformer();
+	            tr.setOutputProperty(OutputKeys.INDENT, "yes");
+	            tr.setOutputProperty(OutputKeys.METHOD, "xml");
+	            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+	            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+	            // send DOM to file
+	            tr.transform(new DOMSource(dom), 
+	                                 new StreamResult(new FileOutputStream(xml)));
+
+	        } catch (TransformerException te) {
+	            System.out.println(te.getMessage());
+	        } catch (IOException ioe) {
+	            System.out.println(ioe.getMessage());
+	        }
+	    } catch (ParserConfigurationException pce) {
+	        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+	    }
+	}
+
 	private static class buildAddPlugin { // Thread safe way to create singleton
 		private static final AddPlugin INSTANCE = new AddPlugin();	
 	}
